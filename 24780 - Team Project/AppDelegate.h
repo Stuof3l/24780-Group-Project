cfc9447e9cb43121cff9e7c9e842a1736fa7e7f7
Stuof3l <zehuaw@andrew.cmu.edu>
2022-11-12 17:10:52 -0500

    Initial Commit

---
+++ 24780 - Team Project/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  24780 - Team Project
+//
+//  Created by Neil on 11/12/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
